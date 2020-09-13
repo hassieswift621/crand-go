d37d3cfe58654b9ca61498d37f9dedeedd2467d2
Hassie <hassie.dev@gmail.com>
2020-09-13 12:05:32 +0100

    Add crand.go

---
+++ crand.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/rand"
+	"golang.org/x/xerrors"
+	"math/big"
+)
+
+const (
+	defaultCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+)
+
+// String generates a crypto secure random string of length size using the default charset.
+func String(size int) (string, error) {
+	return generate(size, defaultCharset)
+}
+
+// StringWithCharset generates a crypto secure random string of length size using the specified charset.
+func StringWithCharset(size int, charset string) (string, error) {
+	return generate(size, charset)
+}
+
+func generate(size int, charset string) (string, error) {
+	b := make([]byte, size)
+
+	for i := 0; i < size; i++ {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", xerrors.Errorf("generate string: %w", err)
+		}
+
+		b[i] = charset[n.Int64()]
+	}
+
+	return string(b), nil
+}
